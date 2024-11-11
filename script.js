79ae9179a18921d7e97391fe1c142ad21c9c6235
sarda qayum <sardarqayu321@gmail.com>
2024-11-11 19:33:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from submitting
+    alert("Thank you for reaching out!");
+  });
+  
